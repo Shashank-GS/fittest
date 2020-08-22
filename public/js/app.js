d4fc9b1f5c3cd918f10d43e05abf55110b688019
Shivam Naik <shivamnaik8239@gmail.com>
2020-08-22 17:24:49 +0530

    added feature to dynamically display sections based on users interaction

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+// display only home on load
+displayComponent("#home");
+
+// Display appropriate component when user clicks
+$navlinks = document.querySelectorAll("nav .nav-link li a");
+$navlinks.forEach((link) => {
+	link.addEventListener("click", (e) => {
+		e.preventDefault();
+		const id = e.target.getAttribute("href");
+		displayComponent(id);
+	});
+});
+
+// display component function
+function displayComponent(id) {
+	$components = document.querySelectorAll("section.isComponent");
+
+	$components.forEach((component) => {
+		component.style.display = "none";
+	});
+
+	$component = document.querySelector(id).parentElement;
+	$component.style.display = "block";
+}
